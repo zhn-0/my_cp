77096287f50a93df93a584ffeef998f557d1d3b9
zhn-0 <1424132877@qq.com>
2022-11-17 19:30:14 +0800

    main

--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@
 
 int main(int argc, char *argv[])
 {
+    printf("\033c");
     char src[256], dst[256], pwd[256];
     int op=0;
     puts("Please input 1 for Backup or 2 for Recover");
     scanf("%d", &op);
+    printf("\033c");
     if(op==1)
     {
         while(1)
@@ -18,9 +20,9 @@ int main(int argc, char *argv[])
             puts("Please input the file you want to backup:");
             scanf("%s", src);
             struct stat st;
+            printf("\033c");
             if(lstat(src, &st)==0)break;
-            fflush(stdout);
-            printf("\n%s not exist, please retry\n", src);
+            printf("%s not exist, please retry\n", src);
         }
         puts("Please input the position you want to save the backup file:");
         scanf("%s", dst);
@@ -29,6 +31,7 @@ int main(int argc, char *argv[])
             fprintf(stderr, "src and dst can not be same\n");
             exit(EXIT_FAILURE);
         }
+        printf("\033c");
         puts("Please input the password that will be used in the encrypto:");
         scanf("%s", pwd);
         generateAesKey((unsigned char*)pwd);
@@ -36,8 +39,16 @@ int main(int argc, char *argv[])
     }
     else if(op==2)
     {
-        puts("Please input the file you want to recover:");
-        scanf("%s", src);
+        while(1)
+        {
+            puts("Please input the file you want to recover:");
+            scanf("%s", src);
+            struct stat st;
+            printf("\033c");
+            if(lstat(src, &st)==0)break;
+            printf("%s not exist, please retry\n", src);
+        }
+        printf("\033c");
         puts("Please input the decrypto password:");
         scanf("%s", pwd);
         generateAesKey((unsigned char *)pwd);
